adddc05cf274b990e0778626ce6cfa530244b871
Clavvv <Schpeen@proton.me>
2024-12-06 10:32:25 -0800

    fixed link not working correctly

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -13,7 +13,7 @@ export default function Navigation() {
         <nav className='top-0 left-0 w-full text-white shadow-md z-10'>
             <div className="max-w-7xl mx-10 px-4 pt-5 flex justify-start items-center place-items-center">
                 <div className="space-x-10 mx-10">
-                    <MdHome className='inline-flex h-full pb-0.5' size={25} />
+                    <MdHome href='/' className='inline-flex h-full pb-0.5' size={25} />
                     <a href="/products" className="h-full inline-flex items-center pt-1">Products</a>
                     <a href="/users" className="h-full inline-flex items-center pt-1">Users</a>
                     <a href='/transactions' className='h-full inline-flex items-center pt-1'>Transactions</a>
